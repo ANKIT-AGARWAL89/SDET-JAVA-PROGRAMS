e62d648bd519a675b8f1ed2b60704f6f81da4309
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-08-30 09:36:43 +0100

    Added the sum of digits programs

---
+++ src/MathematicalPrograms/SumOfDigits.java
@@ -0,0 +1,13 @@
+package MathematicalPrograms;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = 3294;
+        int sum = 0;
+        while (num!=0) {
+            sum = sum + num % 10;
+            num = num/10;
+        }
+        System.out.println(sum);
+    }
+}
