aae673b6c85e7cfa8618cc7f4827540a84c342a6
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-08-30 17:26:57 +0100

    modified the number of digits program to handle the zero and negative number

--- src/MathematicalPrograms/NumberOfDigitsInaNumber.java
+++ src/MathematicalPrograms/NumberOfDigitsInaNumber.java
@@ -1,12 +1,21 @@
 package MathematicalPrograms;
 
+import java.util.Scanner;
+
 public class NumberOfDigitsInaNumber {
     public static void main(String[] args) {
-        long num = 832897832713l;
-        int count=0;
-        while (num!=0){
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        int count = 0;
+        if (num < 0) {
+            num = num * -1;
+        } else if (num == 0) {
+            num = 1;
+        }
+
+        while (num != 0) {
             count++;
-            num/=10;
+            num /= 10;
         }
         System.out.println(count);
     }
