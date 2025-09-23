bfb4b0efc5b9a591f5f27e2b845add34d1d19ba0
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-09-23 09:36:37 +0100

    Adding the code for Second Max element in an array

--- src/StringsJavaPrograms/ReverseTheString.java
+++ src/StringsJavaPrograms/ReverseTheString.java
@@ -14,14 +14,14 @@ public class ReverseTheString {
 //        System.out.println(rev);
 
         //Logic 2: Using the CharArray
-//        char[] arrayOfChar = str.toCharArray();
-//        for (int i = arrayOfChar.length - 1; i >= 0; i--) {
-//            rev = rev + arrayOfChar[i];
-//        }
-//        System.out.println(rev);
+        char[] arrayOfChar = str.toCharArray();
+        for (int i = arrayOfChar.length - 1; i >= 0; i--) {
+            rev = rev + arrayOfChar[i];
+        }
+        System.out.println(rev);
 
         //Logic 3: Using the stringBuilder reverse method
-        StringBuilder stringBuilder = new StringBuilder(str);
-        System.out.println(stringBuilder.reverse());
+//        StringBuilder stringBuilder = new StringBuilder(str);
+//        System.out.println(stringBuilder.reverse());
     }
 }
