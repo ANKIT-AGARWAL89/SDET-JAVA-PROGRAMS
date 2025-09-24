1679d0cbdeac9512a0d63347f9498d12cfe92273
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-09-24 13:08:05 +0100

    Adding the code for min and max element in an array

---
+++ src/ArrayJavaPrograms/MaxandMinElement.java
@@ -0,0 +1,19 @@
+package ArrayJavaPrograms;
+
+public class MaxandMinElement {
+    public static void main(String[] args) {
+        int[] arr = {-2, 4, -1, 5, 2, -4, 9, 21, 1, 21};
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
+}
