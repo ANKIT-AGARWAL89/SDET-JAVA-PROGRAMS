739c666b2c13256b8ed7730b9aee20f78fd094aa
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-09-27 11:07:53 +0100

    Modified the LeadersInArray program

--- src/ArrayJavaPrograms/LeadersInArray.java
+++ src/ArrayJavaPrograms/LeadersInArray.java
@@ -1,20 +1,21 @@
 package ArrayJavaPrograms;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.HashSet;
+
+import java.util.Set;
 
 public class LeadersInArray {
     public static void main(String[] args) {
         int[] arr = {8, 2, 4, 7, 5, 3, 2};
-        List<Integer> leaders = new ArrayList<>();
+        Set<Integer> leaders = new HashSet<>();
         int max = arr[arr.length - 1];
-        leaders.add(max);
-        for (int i = arr.length - 2; i >= 0; i--) {
-            if (arr[i] > max) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] >= max) {
                 max = arr[i];
                 leaders.add(max);
             }
         }
-        System.out.println(leaders.reversed());
+        System.out.println(leaders);
     }
 }
