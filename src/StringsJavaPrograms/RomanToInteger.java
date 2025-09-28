797659705ae1996aafc93e6bad56da4691465da7
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-09-28 11:54:59 +0100

    Adding the program for Roman to Integer

---
+++ src/StringsJavaPrograms/RomanToInteger.java
@@ -0,0 +1,31 @@
+package StringsJavaPrograms;
+
+import java.beans.PropertyEditorSupport;
+import java.util.HashMap;
+
+public class RomanToInteger {
+
+    public static int rti(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        int result = map.get(str.charAt(str.length() - 1));
+        for (int i = str.length() - 2; i >= 0; i--) {
+            if (map.get(str.charAt(i)) < map.get(str.charAt(i + 1))) {
+                result = result - map.get(str.charAt(i));
+            } else result = result + map.get(str.charAt(i));
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        String str = "VXL";
+        System.out.println(rti(str));
+    }
+}
